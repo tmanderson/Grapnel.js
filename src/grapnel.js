2a237e621e0e290afccf9259049ac911860742c8
Mitch Anderson <thomasmitchellanderson@gmail.com>
2014-11-04 15:26:19 -0600

    No this is the new one

--- src/grapnel.js
+++ src/grapnel.js
@@ -18,7 +18,7 @@
         this.events = {}; // Event Listeners
         this.params = []; // Named parameters
         this.state = null; // Event state
-        this.version = '0.5'; // Version
+        this.version = '0.5.0'; // Version
         // Anchor
         this.anchor = {
             defaultHash : config.pushState ? window.location.href.replace(window.location.pathname, '/') : window.location.hash,
